055f5826d8d8458f6b1683d2c5943ad24a41e345
JessicaML <jessicamaryleach@gmail.com>
2023-05-04 11:44:34 +0100

    Create boilerplate
    Create React/Typescript boilerplate with React Testing Library.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App" data-testid="container"><h1>Weather App</h1>
+    </div>
+  );
+}
